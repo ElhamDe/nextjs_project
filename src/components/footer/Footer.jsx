3891f52987167c35f69ee6284342c0ab2d2b2852
Elham Dermaku <dermaku12@gmail.com>
2023-12-04 22:17:56 +0100

    portfolio page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div>©2023 Lamamia. All rights reserved.</div>
+      <div>©2023 ED. All rights reserved.</div>
       <div className={styles.social}>
         <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Elham Dev FB"/>
         <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Elham Dev Insta"/>
