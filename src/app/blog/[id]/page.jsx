2c6635daebc8389440f06cb943d969a05fb63863
Elham Dermaku <dermaku12@gmail.com>
2023-12-05 22:19:38 +0100

    data fetching api

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import styles from './page.module.css';
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
-const BlogPost = () => {
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
+
+  if(!res.ok) {
+    return notFound();
+  }
+
+  return res.json();
+}
+
+
+const BlogPost = async ({params}) => {
+
+  const data = await getData(params.id)
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, totam.
+            {data.title}
           </h1>
           <p className={styles.desc}>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et nihil, aperiam hic minima excepturi officiis, aut suscipit commodi quibusdam molestiae quo, animi id molestias ipsam facere eaque maiores delectus. Saepe ipsam veniam aut! Labore, atque quod ducimus molestias magnam culpa!
