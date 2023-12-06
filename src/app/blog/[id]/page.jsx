b98ad23c0792926b0f194c4dd8207ed982d3838c
Elham Dermaku <dermaku12@gmail.com>
2023-12-06 22:44:14 +0100

    add API posts

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
 async function getData(id) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: "no-store",
   });
 
@@ -16,47 +16,43 @@ async function getData(id) {
 }
 
 
-const BlogPost = async ({params}) => {
-
-  const data = await getData(params.id)
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>
-            {data.title}
-          </h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et nihil, aperiam hic minima excepturi officiis, aut suscipit commodi quibusdam molestiae quo, animi id molestias ipsam facere eaque maiores delectus. Saepe ipsam veniam aut! Labore, atque quod ducimus molestias magnam culpa!
+            {data.desc}
           </p>
           <div className={styles.author}>
             <Image
-              src="https://images.pexels.com/photos/18111290/pexels-photo-18111290/free-photo-of-ornamented-wall-of-building-in-city.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src={data.img}
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>Elham Dermaku</span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
-            <Image
-              src="https://images.pexels.com/photos/18111290/pexels-photo-18111290/free-photo-of-ornamented-wall-of-building-in-city.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt=""
-              fill={true}
-              className={styles.image}
-            />
-            <span className={styles.username}>Elham Dermaku</span>
-          </div>
+          <Image
+            src={data.img}
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro magni tenetur quaerat at iure ipsa, asperiores aliquam, sunt optio ipsam nisi possimus ad nulla, corporis sapiente est voluptate voluptatum deserunt eaque iste numquam iusto. Esse sequi repudiandae cum excepturi omnis assumenda dolorem dicta voluptas iure quia aliquid, perferendis ab amet.
+         {data.content}
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  const res = await fetch("http://localhost:3000/api/posts", {
     cache: "no-store",
   });
 
@@ -21,10 +21,10 @@ const Blog = async () => {
   return (
     <div className={styles.mainContainer}>
       {data.map((item) => (
-        <Link href="/blog/testId" className={styles.container}>
+        <Link href={`/blog/${item._id}`} className={styles.container}>
           <div className={styles.imageContainer}>
             <Image
-              src="https://images.pexels.com/photos/18111290/pexels-photo-18111290/free-photo-of-ornamented-wall-of-building-in-city.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src={item.img}
               alt=""
               width={400}
               height={250}
@@ -33,7 +33,7 @@ const Blog = async () => {
           </div>
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>Desc</p>
+            <p className={styles.desc}>{item.desc}</p>
           </div>
         </Link>
       ))}
